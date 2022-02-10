c4548f548747acac32e6c1b1449ef3c30dd625da
vitkovsky21 <vladbekkoshkarov@yandex.ru>
2022-02-10 20:54:08 +0300

    first assignment commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,13 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
