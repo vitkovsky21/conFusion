8b9a69ebdcec0dca475a5cac4af3c778540afcee
vitkovsky21 <vladbekkoshkarov@yandex.ru>
2022-02-10 18:06:15 +0300

    Components Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
@@ -22,6 +25,9 @@ import { MenuComponent } from './menu/menu.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatGridListModule,
     FlexLayoutModule
   ],
   providers: [],
