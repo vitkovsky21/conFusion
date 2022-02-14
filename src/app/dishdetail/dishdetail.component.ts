28baacc7f0ef00471ef9313b8e1d0579e033427c
vitkovsky21 <vladbekkoshkarov@yandex.ru>
2022-02-14 20:25:31 +0300

    Promise Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -3,6 +3,7 @@ import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { DISHES } from '../shared/dishes';
 
 
 @Component({
@@ -15,13 +16,14 @@ export class DishdetailComponent implements OnInit {
 
   dish!: Dish;
 
-  constructor(private dishservice: DishService,
+  constructor(private dishService: DishService,
     private route: ActivatedRoute,
     private location: Location) { }
 
   ngOnInit() {
     const id: any = this.route.snapshot.params['id'];
-    this.dish = this.dishservice.getDish(id);
+    this.dishService.getDish(id)
+      .then(dish => this.dish = dish)
   }
 
   goBack(): void {
